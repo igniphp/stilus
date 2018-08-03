cc3faa4128af1b54d964878578d49d3720e97320
Dawid <dawid.kraczkowski@gmail.com>
2018-08-03 21:46:19 +0200

    Vue.js configuration

--- vue.config.js
+++ vue.config.js
@@ -3,24 +3,14 @@ var yaml = require('read-yaml');
 var config = yaml.sync('.stilus.yml');
 
 module.exports = {
-    outputDir: config.development.build_dir,
+    outputDir: config.development.build.dir,
     devServer: {
         port: config.development.web_server.port || 8080,
         host: config.development.web_server.host || "0.0.0.0",
     },
-    assetsDir: 'assets',
     css: {
-        sourceMap: config.development.css_source_map || true,
+        sourceMap: config.development.build.css_source_map || true,
         extract: false
     },
-    pages: {
-        index: {
-            // entry for the page
-            entry: 'src/Stilus.js',
-            // the source template
-            template: 'src/index.html',
-            // output as dist/index.html
-            filename: 'index.html',
-        },
-    }
+    pages: config.development.build.pages,
 };
